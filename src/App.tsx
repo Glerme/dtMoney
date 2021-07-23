0965357a06e6217d4ff89b4e513dca3024429ffd
guilherme <guilherme.godoi@allcomsistemas.com.br>
2021-07-23 08:25:11 -0300

    :sparkles: Terminado o app

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { useCallback, useState } from "react";
+import { GlobalStyle } from "./styles/global";
+
+import { Header } from "./components/Header";
+import { Dashboard } from "./components/Dashboard";
+import NewTransactionModal from "./components/NewTransactionModal";
+import { TransactionsProvider } from "./hooks/useTransactions";
+
+const App = () => {
+  const [isModalTransactionOpen, setIsModalTransactionOpen] = useState(false);
+
+  const handleOpenModal = useCallback(() => {
+    setIsModalTransactionOpen(true);
+  }, []);
+
+  const handleCloseModal = useCallback(() => {
+    setIsModalTransactionOpen(false);
+  }, []);
+
+  return (
+    <TransactionsProvider>
+      <Header openModal={handleOpenModal} />
+      <Dashboard />
+      <GlobalStyle />
+
+      <NewTransactionModal
+        isOpen={isModalTransactionOpen}
+        onRequestClose={handleCloseModal}
+      />
+    </TransactionsProvider>
+  );
+};
+
+export default App;
---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Summary } from "../Summary";
+import { TransactionsTable } from "../TransactionsTable";
+
+import { Container } from "./styles";
+
+const Dashboard: React.FC = () => {
+  return (
+    <Container>
+      <Summary />
+      <TransactionsTable />
+    </Container>
+  );
+};
+
+export { Dashboard };
